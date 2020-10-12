d146e906c4d5a5ccd4938b100836a5ed1eb9735c
IT521 <terrysfrancis@hotmail.com>
2020-10-11 21:37:52 -0400

    Fix text size selection logic

--- src/components/awc-name/awc-name.tsx
+++ src/components/awc-name/awc-name.tsx
@@ -5,85 +5,87 @@ import { SIZE } from './awc-name.types';
 const SIZES = ['xsmall', 'small', 'medium', 'large'];
 
 @Component({
-  tag: 'awc-name',
-  styleUrl: 'awc-name.scss',
-  shadow: true,
+    tag: 'awc-name',
+    styleUrl: 'awc-name.scss',
+    shadow: true,
 })
 export class AwcName {
-  /**
-   * First name
-   */
-  @Prop({ reflect: true }) first: string;
+    /**
+     * First name
+     */
+    @Prop({ reflect: true }) first: string;
 
-  /**
-   * Middle name
-   */
-  @Prop({ reflect: true }) middle: string;
+    /**
+     * Middle name
+     */
+    @Prop({ reflect: true }) middle: string;
 
-  /**
-   * Last name
-   */
-  @Prop({ reflect: true }) last: string;
+    /**
+     * Last name
+     */
+    @Prop({ reflect: true }) last: string;
 
-  /**
-   * Text size
-   */
-  @Prop({ reflect: true, mutable: true }) size: SIZE = 'medium';
+    /**
+     * Text size
+     */
+    @Prop({ reflect: true, mutable: true }) size: SIZE = 'medium';
 
-  /**
-   * Has Greeting
-   */
-  @Prop({ reflect: true }) hasGreeting: boolean = false;
+    /**
+     * Has Greeting
+     */
+    @Prop({ reflect: true }) hasGreeting: boolean = false;
 
-  /**
-   * Greeting
-   */
-  @Prop({ reflect: true }) greeting: string = 'Hello, World! I am';
+    /**
+     * Greeting
+     */
+    @Prop({ reflect: true }) greeting: string = 'Hello, World! I am';
 
-  /**
-   * Has size button
-   */
-  @Prop({ reflect: true }) hasSizeButton: boolean = false;
+    /**
+     * Has size button
+     */
+    @Prop({ reflect: true }) hasSizeButton: boolean = false;
 
-  @Listen('click', { capture: true })
-  handleClick() {
-    let index = SIZES.findIndex(size => size === this.size);
-    index = index === 0 ? index + 1 : index - 1;
-    this.size = SIZES[index] as SIZE;
-    console.log('Text size set to:', this.size);
-  }
+    @Listen('click', { capture: true })
+    handleClick() {
+        let randomItem: SIZE = this.size;
+        do {
+            randomItem = SIZES[Math.floor(Math.random() * SIZES.length)] as SIZE;
+        } while (randomItem === this.size);
+        this.size = randomItem as SIZE;
+        console.log('Text size set to:', this.size);
+    }
 
-  private getCss(): string {
-    return `awc-name awc-name__text--${this.size}`;
-  }
+    private getCss(): string {
+        return `awc-name awc-name__text--${this.size}`;
+    }
 
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
-  }
+    private getText(): string {
+        return format(this.first, this.middle, this.last);
+    }
 
-  private renderButton() {
-    return this.hasSizeButton ? <awc-button text="Change Text Size" color="blue" size="xsmall" margin="medium" /> : null;
-  }
+    private renderButton() {
+        return this.hasSizeButton ? <awc-button text="Change Text Size" color="blue" size="xsmall" margin="medium" /> : null;
+    }
 
-  private renderName() {
-    const css = this.getCss();
+    private renderName() {
+        const css = this.getCss();
 
-    return this.hasGreeting ? (
-      <div class={css}>
-        <div>
-          {this.greeting} {this.getText()}
-        </div>
-        {this.renderButton()}
-      </div>
-    ) : (
-      <div class={css}>
-        <div>{this.getText()}</div>
-        {this.renderButton()}
-      </div>
-    );
-  }
+        return this.hasGreeting ? (
+            <div class={css}>
+                <div>
+                    {this.greeting} {this.getText()}
+                </div>
+                {this.renderButton()}
+            </div>
+        ) : (
+            <div class={css}>
+                <div>{this.getText()}</div>
+                {this.renderButton()}
+            </div>
+        );
+    }
 
-  render() {
-    return this.renderName();
-  }
+    render() {
+        return this.renderName();
+    }
 }
